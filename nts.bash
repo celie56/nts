3f462bdba7772d72744fbb49abd10d731a3ec8e9
celie56 <celie56@gmail.com>
2022-12-28 21:19:55 -0500

    add Overview section

--- nts.bash
+++ nts.bash
@@ -1,16 +1,47 @@
 
-##########################%%%%%%%%%%%%%%%%%%%#
-                         # Table of contents #
-##########################%%%%%%%%%%%%%%%%%%%#
+########################################################%%%%%%%%%%%%%%%%%%%#
+                                                       # Table of contents #
+########################################################%%%%%%%%%%%%%%%%%%%#
 
+# 0. Overview
 # 1. Constant
 # 2. Helpers
 # 3. Drafting
 # 4. Review
 
-###################################%%%%%%%%%%#
-                                  # Constant #
-###################################%%%%%%%%%%#
+########################################################%%%%%%%%%%%%%%%%%%%#
+                                                                # Overview #
+########################################################%%%%%%%%%%%%%%%%%%%#
+
+# I like to have a consistent note taking system across computers
+# I also like that system to be as automated as possible
+# Oh and I want it to be totally custom because because
+
+# The three main functions I use are:
+#  1. now()
+#   * take me to a dated markdown file for me to get started jotting ideas
+#  2. temp()
+#   * take me to a new, dated folder where I might put multiple things in
+#  3. recap()
+#   * review recent notes (only from now() for now)
+
+# Future:
+# * organization of existing now() and temp() files
+#   * I have accumulated many and there is overlap that would make sense
+#     to be put together
+# * projects
+#   * this might be a way to organize the above...
+#     but I also want to "define" projects
+
+# Structure of my current ~/notes dir
+# * daily
+# * temps
+# * topics      (a form of organization -- but I create these individually)
+# * task_notes  (old notes associated with taskwarrior tasks)
+
+#################################################################%%%%%%%%%%#
+                                                                # Constant #
+#################################################################%%%%%%%%%%#
 
 if [[ "$NTS_DIR_DAILY" == "" ]]; then
   NTS_DIR_DAILY=~/notes/daily
@@ -20,17 +51,17 @@ if [[ "$NTS_DIR_TEMP" == "" ]]; then
   NTS_DIR_TEMP=~/notes/temps
 fi
 
-###################################%%%%%%%%%%#
-                                  # Helpers  #
-###################################%%%%%%%%%%#
+#################################################################%%%%%%%%%%#
+                                                                # Helpers  #
+#################################################################%%%%%%%%%%#
 
 today() {
   echo $(date "+%Y-%m-%d")
 }
 
-###################################%%%%%%%%%%#
-                                  # Drafting #
-###################################%%%%%%%%%%#
+#################################################################%%%%%%%%%%#
+                                                                # Drafting #
+#################################################################%%%%%%%%%%#
 
 wnow() {
   echo ${NTS_DIR_DAILY}/$(today).md
@@ -63,9 +94,9 @@ gotemp() {
 }
 
 
-###################################%%%%%%%%%%#
-                                  # Review   #
-###################################%%%%%%%%%%#
+#################################################################%%%%%%%%%%#
+                                                                # Review   #
+#################################################################%%%%%%%%%%#
 
 recap() {                
   if [[ $# -gt 0 ]]; then
